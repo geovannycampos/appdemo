9d78e8d2681a9d1d82693bc2f8ec18b19e43f04a
geovannycampos <geovanny.campos@itca.edu.sv>
2016-11-12 18:28:34 -0600

    cambio1
    se cambio palabra

--- src/appdemo/Formulario1.java
+++ src/appdemo/Formulario1.java
@@ -77,7 +77,7 @@ public class Formulario1 extends javax.swing.JFrame {
 
     private void btnMostrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMostrarActionPerformed
         // TODO add your handling code here:
-        JOptionPane.showMessageDialog(this, "el numero ingresado es: " + txtNumero.getText());
+        JOptionPane.showMessageDialog(this, "el numero ingresado fue: " + txtNumero.getText());
     }//GEN-LAST:event_btnMostrarActionPerformed
 
     /**
